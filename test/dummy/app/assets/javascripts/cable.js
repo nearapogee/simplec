4f5fde8601c0aba2346af7b1366aae2f22449e14
Matt Smith <matt@nearapogee.com>
2017-08-15 15:48:03 -0600

    disable action cable completely.

--- test/dummy/app/assets/javascripts/cable.js
+++ test/dummy/app/assets/javascripts/cable.js
@@ -2,12 +2,12 @@
 // You can generate new channels where WebSocket features live using the `rails generate channel` command.
 //
 // require action_cable
-//= require_self
-//= require_tree ./channels
+// require_self
+// require_tree ./channels
 
-(function() {
-  this.App || (this.App = {});
-
-  App.cable = ActionCable.createConsumer();
-
-}).call(this);
+//(function() {
+//  this.App || (this.App = {});
+//
+//  App.cable = ActionCable.createConsumer();
+//
+//}).call(this);
