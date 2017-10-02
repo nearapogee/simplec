e9ff617b7721ae255d7462c3a0181b03f3fb1c79
Matt Smith <matt@nearapogee.com>
2017-10-02 17:10:15 -0600

    fix slug validation on documents and sets.

--- app/models/simplec/document.rb
+++ app/models/simplec/document.rb
@@ -16,6 +16,14 @@ module Simplec
       self.file.url
     end
 
+    # @!visibility private
+    module Normalizers
+      def slug=(val)
+        super val.blank? ? nil : val
+      end
+    end
+    prepend Normalizers
+
     private
 
     def validate_subdomain_or_set!
@@ -25,13 +33,13 @@ module Simplec
     end
 
     def validate_slug!
-      return if self.slug.nil?
+      return if self.slug.blank?
       similar = self.class.where(slug: self.slug).where.not(id: self.id)
       return if similar.size == 0
       return if (
         similar.map(&:subdomain_ids).flatten & self.subdomain_ids
       ).length == 0
-      errors.add :slug, 'slug must be unique across linked subdomains'
+      errors.add :slug, 'must be unique across linked subdomains'
     end
 
   end
--- app/models/simplec/document_set.rb
+++ app/models/simplec/document_set.rb
@@ -9,6 +9,14 @@ module Simplec
     validate :validate_subdomain!
     validate :validate_slug!
 
+    # @!visibility private
+    module Normalizers
+      def slug=(val)
+        super val.blank? ? nil : val
+      end
+    end
+    prepend Normalizers
+
     private
 
     def validate_subdomain!
@@ -17,12 +25,13 @@ module Simplec
     end
 
     def validate_slug!
+      return if self.slug.blank?
       similar = self.class.where(slug: self.slug).where.not(id: self.id)
       return if similar.size == 0
       return if (
         similar.map(&:subdomain_ids).flatten & self.subdomain_ids
       ).length == 0
-      errors.add :slug, 'slug must be unique across linked subdomains'
+      errors.add :slug, 'must be unique across linked subdomains'
     end
 
   end
