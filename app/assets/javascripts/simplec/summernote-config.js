bcd67b7898e77c52486688322d042cc3b1c41c46
Matt Smith <matt@nearapogee.com>
2018-05-04 13:32:23 -0600

    fix summernote initialization.

--- app/assets/javascripts/simplec/summernote-config.js
+++ app/assets/javascripts/simplec/summernote-config.js
@@ -25,59 +25,65 @@ $.summernote.options.toolbar = [
 if (!window.simplec) window.simplec = {};
 
 window.simplec.initSummernote = function() {
-  window.simplec.summernoteInitialized = true;
+  $('.editor-field').each(function(idx, el) {
+    var $el = $(el);
 
-  $('.editor-field').on('summernote.init', function(event) {
+    if ($el.data('simplec') !== 'initialized') {
+      console.log('adding summernote', el);
 
-    // Material Kit Hack
-    //$('.note-toolbar .btn-default').
-    //  removeClass('btn-default').addClass('btn-simple');
+      $el.on('summernote.init', function(event) {
+        // Material Kit Hack
+        //$('.note-toolbar .btn-default').
+        //  removeClass('btn-default').addClass('btn-simple');
 
-    //$('.note-popover').css({'display': 'none'}); // BS4 HACK
-    var $editor = $(event.target),
-        $input = $($editor.data('input'));
-    if ($input.val().length > 0) $editor.summernote('code', $input.val());
-  }).on('summernote.change', function(event, contents, $editable) {
-    var $editor = $(event.target),
-        $input = $($editor.data('input'));
-    $input.val(contents);
-  }.bind(window)).on('summernote.image.upload', function(event, files) {
-    console.log('upload event');
-    for (var i = 0; i < files.length; i++) {
-      var file = files[i],
-          reader = new FileReader();
-      reader.onloadend = function() {
-        $.ajax({
-          method: 'POST',
-          url: '/embedded-images',
-          dataType: 'json',
-          contentType: 'application/json',
-          headers: { 'X-Engine': 'simplec' },
-          data: JSON.stringify({
-            asset_url: reader.result,
-            asset_name: file.name
-          })
-        }).fail(function(data, textStatus, jqXHR){
-          console.log('IMAGE UPLOAD FAIL', textStatus);
-        }).done(function(data, textStatus, jqXHR){
-          $(event.target).summernote(
-            'insertImage',
-            jqXHR.responseJSON.asset_url, jqXHR.responseJSON.asset_name
-          );
-        });
-      };
-      reader.readAsDataURL(file);
+        //$('.note-popover').css({'display': 'none'}); // BS4 HACK
+        var $editor = $(event.target),
+            $input = $($editor.data('input'));
+        if ($input.val().length > 0) $editor.summernote('code', $input.val());
+      }).on('summernote.change', function(event, contents, $editable) {
+        var $editor = $(event.target),
+            $input = $($editor.data('input'));
+        $input.val(contents);
+      }.bind(window)).on('summernote.image.upload', function(event, files) {
+        console.log('upload event');
+        for (var i = 0; i < files.length; i++) {
+          var file = files[i],
+              reader = new FileReader();
+          reader.onloadend = function() {
+            $.ajax({
+              method: 'POST',
+              url: '/embedded-images',
+              dataType: 'json',
+              contentType: 'application/json',
+              headers: { 'X-Engine': 'simplec' },
+              data: JSON.stringify({
+                asset_url: reader.result,
+                asset_name: file.name
+              })
+            }).fail(function(data, textStatus, jqXHR){
+              console.log('IMAGE UPLOAD FAIL', textStatus);
+            }).done(function(data, textStatus, jqXHR){
+              $(event.target).summernote(
+                'insertImage',
+                jqXHR.responseJSON.asset_url, jqXHR.responseJSON.asset_name
+              );
+            });
+          };
+          reader.readAsDataURL(file);
+        }
+      });
+
+      $el.summernote({
+        height: 500,
+        minHeight: 100,
+        maxHeight: 1200
+      });
+
+      $el.attr('data-simplec', 'initialized');
     }
   });
-
-  $('.editor-field').summernote({
-    height: 500,
-    minHeight: 100,
-    maxHeight: 1200
-  });
-
 };
 
 $(document).on('ready turbolinks:load', function() {
-  if (!window.simplec.summernoteInitialized) window.simplec.initSummernote();
+  window.simplec.initSummernote();
 });
